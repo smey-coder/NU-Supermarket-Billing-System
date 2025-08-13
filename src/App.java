0b9b80ebf445539f0206f6ce652db186f41f34a1
Your Name <sanreaksmey01@gmail.com>
2025-08-13 13:46:42 +0700

    main Test

--- src/App.java
+++ src/App.java
@@ -6,6 +6,7 @@ public class App extends Login_Form{
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             new Login_Form().setVisible(true);
+            
         });
     }
 }
